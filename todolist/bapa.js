eec797c7829e8fef33e58039fbfe2af61aa3302d
Sambedan Sahoo <138988501+sambedan1@users.noreply.github.com>
2023-07-26 14:37:41 +0530

    Add files via upload

---
+++ todolist/bapa.js
@@ -0,0 +1,44 @@
+const inputBox=document.getElementById("input-box");
+const ListContainer=document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value=='')
+    {
+        alert("you must write something");
+
+    }
+    else
+    {
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        ListContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value="";
+    saveData();
+}
+ListContainer.addEventListener("click",function(e){
+    if(e.target.tagName=="LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName=="SPAN")
+    {
+e.target.parentElement.remove();
+saveData();
+    }
+
+},false);
+
+function saveData()
+{
+    localStorage.setItem("data",ListContainer.innerHTML);
+}
+function showTask()
+{
+    ListContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
